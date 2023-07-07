fabb3ceb68cb7788323b3af57c48e1fde892b228
Brigham Chen <83602138+brigham4210@users.noreply.github.com>
2023-07-07 00:37:44 -0600

    c

--- Python/Data.py
+++ Python/Data.py
@@ -1,10 +1,8 @@
 import json
 
-with open("./../Json/maps.json", "r", encoding="utf-8") as f:
+with open("Json/mappers.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 one = data["one"]
 two = data["two"]
 
-# with open("two.json", "r", encoding="utf-8") as f:
-#     two = json.load(f)
--- Python/Logo.py
+++ Python/Logo.py
@@ -2,7 +2,7 @@ from kivy.uix.image import Image
 
 
 logo = Image(
-    source="../Pics/logo.png",
+    source="Pics/logo.png",
     size_hint=(.15, .15),
     pos_hint={'center_x': .5, 'center_y': .8}
 )
