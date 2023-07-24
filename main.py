aff4a5c36eb02bc6ebdb64ce31f615cd417328e4
Brigham Chen <83602138+brigham4210@users.noreply.github.com>
2023-07-24 15:35:52 -0600

    coder class

--- main.py
+++ main.py
@@ -1,41 +1,5 @@
-from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from Python.Logo import Logo
-from Python.Copy import copy_button
-from Python.TextBox import textBox
-from Python.Encode import encode_button
-from Python.Decode import decode_button
-from Python.OutputBox import output_box
-
-
-class Body(FloatLayout):
-    def __init__(self, **kwargs):
-        super(Body, self).__init__(**kwargs)
-        self.pos_hint = {"center_x": 0.5, "center_y": 0.5}
-
-
-class SecretCoder(App):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.body = Body()
-        self.logo = Logo("Pics/logo.png")
-        self.intro = output_box
-        self.copy = copy_button
-        self.textBox = textBox
-        self.encode = encode_button
-        self.decode = decode_button
-
-    def build(self):
-        self.body.add_widget(self.logo)
-        self.body.add_widget(self.intro)
-        self.body.add_widget(self.copy)
-        self.body.add_widget(self.textBox)
-        self.body.add_widget(self.encode)
-        self.body.add_widget(self.decode)
-
-        return self.body
-
+from Coder import SecretCoder
 
 # run Class
 if __name__ == "__main__":
-    SecretCoder().run()
+    SecretCoder("Pics/nba.png").run()
